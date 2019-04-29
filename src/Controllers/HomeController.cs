e958045ca787804ae3b8f638f066ceb46f29ef85
Guilherme Fernando Angelico <angelicogfa@gmail.com>
2019-04-29 13:57:09 -0300

    'inclusão'

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using elastic_kibana.Models;
+using Microsoft.Extensions.Logging;
+
+namespace elastic_kibana.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger<HomeController> logger;
+        public HomeController(ILoggerFactory factory)
+        {
+            logger = factory.CreateLogger<HomeController>();    
+        }
+
+        public IActionResult Index()
+        {
+            logger.LogTrace("Index fired");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
